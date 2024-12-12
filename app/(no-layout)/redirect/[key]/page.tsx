3f1b1828c10c17a2406ea6c2ad80e34a2cffdc21
Pierre Lemmel <pierre.lemmel@gmail.com>
2024-12-12 16:33:30 +0100

    Add Clavel

--- app/(no-layout)/redirect/[key]/page.tsx
+++ app/(no-layout)/redirect/[key]/page.tsx
@@ -5,10 +5,12 @@ import { useParams } from 'next/navigation';
 
 const URL_PROARTI = 'https://www.proarti.fr/collect/project/quelque-chose-a-change-dans-lair/0';
 const URL_FACTORY_AVIGNON = 'https://lafactory.vostickets.net/billet/FR/representation-LA_FACTORY-21994-0.wb';
+const URL_CLAVEL = 'https://www.billetweb.fr/qacda-clavel';
 
 const urls: Record<string, string> = {
     'carte-visite': URL_PROARTI,
     'flyer-avignon': URL_FACTORY_AVIGNON,
+    'qacda-clavel': URL_CLAVEL,
     'flyer-cagnotte': URL_PROARTI,
     'kakemono': URL_PROARTI,
 };
