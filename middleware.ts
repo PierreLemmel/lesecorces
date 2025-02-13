2bc40cbc60ca719b237df78d5d5aa11abda5f86d
Pierre Lemmel <pierre.lemmel@gmail.com>
2025-02-13 16:27:35 +0100

    Debug middleware

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,17 @@ import type { NextRequest } from 'next/server'
 
  
 export async function middleware(req: NextRequest) {
-
+console.log(1)
     if (req.nextUrl.pathname.startsWith("/bo")) {
+        console.log(2)
         const pathChunks = req.nextUrl.pathname
             .split("/")
             .filter(c => c !== "");
-
+console.log("pathChunks", pathChunks);
         if (!(pathChunks.length >= 2 && pathChunks[1] === "login")) {
-            
+            console.log(3)
             const sessionCookie = req.cookies.get("session");
-
+console.log("sessionCookie", sessionCookie);
             if (sessionCookie) {
                 const result = await fetch(req.nextUrl.origin + "/api/session/validate", {
                     method: "POST",
@@ -39,14 +40,14 @@ export async function middleware(req: NextRequest) {
             return NextResponse.redirect(redirectUrl);
         }
     }
-
+console.log(66)
     const maintenance = process.env.MAINTENANCE === 'true';
     if (maintenance) {
         const rewriteUrl = req.nextUrl.clone();
         rewriteUrl.pathname = '/maintenance';
         return NextResponse.rewrite(rewriteUrl);
     }
-
+console.log(100)
     return NextResponse.next()
 }
 
