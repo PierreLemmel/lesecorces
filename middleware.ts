f0b666593e13200b2926ebfbb4137d7f7a366e60
Pierre Lemmel <pierre.lemmel@gmail.com>
2025-02-13 17:48:47 +0100

    Debug middleware

--- middleware.ts
+++ middleware.ts
@@ -26,7 +26,6 @@ console.log("sessionCookie", sessionCookie);
 
                 console.log(5)
 console.log(result);
-console.log(await readBody(result.body!))
 
 
                 if (result.status === 200) {
@@ -62,26 +61,3 @@ export const config = {
         '/((?!_next/static|img|fonts|videos|api|redirect|favicon.ico).*)'
     ],
 }
-
-async function readBody(stream: ReadableStream) {
-    const reader = stream.getReader();
-    let result = '';
-  
-    try {
-      while (true) {
-        const { done, value } = await reader.read();
-  
-        if (done) {
-          break; // Stream is finished
-        }
-  
-        // Assuming the stream contains text data encoded as UTF-8
-        const chunk = new TextDecoder().decode(value);
-        result += chunk;
-      }
-    } finally {
-      reader.releaseLock(); // Release the lock when done
-    }
-  
-    return result;
-  }
