48d330e35de4aaecb38ff19b31144756d1e69a13
Pierre Lemmel <pierre.lemmel@gmail.com>
2025-04-11 00:17:30 +0200

    Force update

--- server/admin.ts
+++ server/admin.ts
@@ -2,5 +2,6 @@ import { verifySessionCookie } from "../lib/firebase-server";
 
 export async function verifyThatSessionCookieIsAdmin(token: string) {
     const decoded = await verifySessionCookie(token);
+    
     return decoded.email === process.env.ADMIN_EMAIL;
 }
